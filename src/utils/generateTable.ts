fc8f412444ed514a8c9e44376ae84c5d355b88ce
davinnchii <zaikoivan99@gmail.com>
2023-12-18 13:28:28 +0100

    Implemented same functionality changing state

--- src/utils/generateTable.ts
+++ src/utils/generateTable.ts
@@ -1,3 +1,11 @@
 export function generateTable(field: number) {
-  return new Array(field).fill(null).map(value => new Array(field).fill(false))
+  let id = 0;
+  return new Array(field).fill(null)
+    .map(value => new Array(field).fill(null)
+      .map(value => ({
+          id: id++,
+          value: false
+        })
+      )
+    );
 }
